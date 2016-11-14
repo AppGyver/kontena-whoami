b61bb8153909a27de46c5ae869e4779557553fca
Matti Paksula <matti.paksula@iki.fi>
2016-11-14 11:25:35 +0200

    feat (index) Show instance number in ascii.

--- Gemfile
+++ Gemfile
@@ -1,6 +1,7 @@
 source 'https://rubygems.org'
 
 gem 'sinatra'
+gem 'artii'
 
 group :development do
   gem 'pry'
--- app.rb
+++ app.rb
@@ -1,5 +1,16 @@
 require 'sinatra'
+require 'artii'
+
+def better_hostname
+  return ENV['HOSTNAME'] if ENV['HOSTNAME'].include? "kontena.local"
+
+  "kontena-whoami-99.kontena.local"
+end
 
 get '/' do
-  erb :index
+  a = Artii::Base.new :font => 'graffiti'
+  instance_number = better_hostname.split(".").first.split("-").last
+  erb :index, locals: {
+    instance_number_ascii: a.asciify(instance_number)
+  }
 end
